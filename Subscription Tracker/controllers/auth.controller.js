fe44340dbbd927cb848badb206486de02c0ca5b8
ZiyanRana <mziyanrana@gmail.com>
2026-05-01 21:08:52 +0500

    Working on project: Subscription Tracker

--- Subscription Tracker/controllers/auth.controller.js
+++ Subscription Tracker/controllers/auth.controller.js
@@ -1,5 +1,26 @@
-export const signUp = async (req, res, next) => {
+import mongoose from "mongoose";
+import User from '../models/user.model.js';
 
+export const signUp = async (req, res, next) => {
+    const session = await mongoose.startSession();
+    session.startTransaction()
+
+    try {
+        const {name, email, password} = req.body;
+
+        // Check existing user
+        const existingUser = await User.findOne({email});
+        if (existingUser) {
+            const error = new Error('User already exist!');
+            error.statusCode = 409;
+            throw error;
+        }
+
+        
+    } catch (error) {
+        await session.abortTransaction();
+        session.endSession();
+    }
 };
 
 export const signIn = async (req, res, next) => {
