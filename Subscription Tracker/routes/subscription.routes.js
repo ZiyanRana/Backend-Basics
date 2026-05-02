3319d9b0c00e070401d37773888d49cc7fc2e039
ZiyanRana <mziyanrana@gmail.com>
2026-05-03 00:46:46 +0500

    Working on project: Subscription Tracker

--- Subscription Tracker/routes/subscription.routes.js
+++ Subscription Tracker/routes/subscription.routes.js
@@ -1,4 +1,7 @@
 import {Router} from 'express';
+import { authorize } from '../middlewares/auth.middleware';
+import { createSubscription, getUserSubscriptions } from '../controllers/subscription.controller';
+
 const subscriptionRouter = Router();
 
 subscriptionRouter.get('/', (req, res) => {
@@ -13,9 +16,7 @@ subscriptionRouter.get('/:id', (req, res) => {
     res.send({title: 'GET specific subscription!'});
 });
 
-subscriptionRouter.post('/', (req, res) => {
-    res.send({title: 'CREATE a subscription!'});
-});
+subscriptionRouter.post('/', authorize, createSubscription);
 
 subscriptionRouter.put('/:id', (req, res) => {
     res.send({title: 'UPDATE a subscription!'});
@@ -25,9 +26,7 @@ subscriptionRouter.delete('/:id', (req, res) => {
     res.send({title: 'DELETE a subscription!'});
 });
 
-subscriptionRouter.get('/user/:id', (req, res) => {
-    res.send({title: 'GET all user subscriptions!'});
-});
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
 
 subscriptionRouter.get('/:id/cancel', (req, res) => {
     res.send({title: 'CANCEL a subscription!'});
