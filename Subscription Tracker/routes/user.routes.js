e93b47285a14d69e574ee4ea983538819d7d008f
ZiyanRana <mziyanrana@gmail.com>
2026-05-03 00:25:55 +0500

    Working on project: Subscription Tracker

--- Subscription Tracker/routes/user.routes.js
+++ Subscription Tracker/routes/user.routes.js
@@ -1,13 +1,11 @@
 import {Router} from 'express';
+import { getUsers, getUser } from '../controllers/user.controller.js';
+import { authorize } from '../middlewares/auth.middleware.js';
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => {
-    res.send({title: 'GET all users!'});
-});
+userRouter.get('/', getUsers);
 
-userRouter.get('/:id', (req, res) => {
-    res.send({title: 'GET specific user!'});
-});
+userRouter.get('/:id', authorize, getUser);
 
 userRouter.post('/', (req, res) => {
     res.send({title: 'CREATE a new user!'});
