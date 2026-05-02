ec17996fb79d0a151bf9476d437a767fb72bc6c8
ZiyanRana <mziyanrana@gmail.com>
2026-05-02 21:07:37 +0500

    Working on project: Subscription Tracker

--- Subscription Tracker/controllers/auth.controller.js
+++ Subscription Tracker/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from '../models/user.model.js';
+import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/env.js";
 
 export const signUp = async (req, res, next) => {
     const session = await mongoose.startSession();
@@ -16,10 +19,35 @@ export const signUp = async (req, res, next) => {
             throw error;
         }
 
-        
+        //Hash Password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUsers = await User.create([{name, email, password: hashedPassword}], {session});
+
+        const token = jwt.sign({userId: newUsers[0]._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.status(201).json({
+            success: true,
+            message: 'User created successfully!',
+            data: {
+                token,
+                user: newUsers[0],
+            }
+        })
     } catch (error) {
         await session.abortTransaction();
         session.endSession();
+
+        console.error(error);
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
     }
 };
 
