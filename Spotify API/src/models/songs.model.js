505fc5f04a63d17e28264a2e75b82a0db35fd5a7
ZiyanRana <mziyanrana@gmail.com>
2026-05-21 20:06:19 +0500

    Working on project: Spotify API

---
+++ Spotify API/src/models/songs.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const songsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Song title is required!'],
+        trim: true,
+        minLength: 1,
+        maxLength: 100
+    },
+    cover: {
+        type: String,
+        required: [true, 'Song cover is required!']
+    },
+    audio: {
+        type: String,
+        required: [true, 'Song audio is required!']
+    },
+    artist: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Song artist is required!']
+    }
+}, {timestamps: true});
+
+const SongsModel = mongoose.model('Songs', songsSchema);
+
+export default SongsModel;
