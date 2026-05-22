8cf670defb93e7acc3fbd8436a84bc9fffb3c407
ZiyanRana <mziyanrana@gmail.com>
2026-05-22 23:01:15 +0500

    Upload project: Spotify API

--- Spotify API/src/app.js
+++ Spotify API/src/app.js
@@ -10,5 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/songs', songRouter);
+app.use('/api/v1/albums', albumRouter);
 
 export default app;
