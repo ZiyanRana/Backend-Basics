164872637d5d52e4948ea39e694b1e29c5e7ef9e
ZiyanRana <mziyanrana@gmail.com>
2026-05-22 22:46:08 +0500

    Working on project: Spotify API

--- Spotify API/src/controllers/album.controller.js
+++ Spotify API/src/controllers/album.controller.js
@@ -124,4 +124,24 @@ export const deleteAlbum = async (req, res) => {
     if (!album) {
         return res.status(404).json({ message: 'Album not found!' });
     }
+
+    if (album.artist.toString() !== user._id.toString()) {
+        return res.status(403).json({ message: 'You can only delete your own albums!' });
+    }
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+        await AlbumModel.deleteOne({ _id: id }, { session });
+        deleteFile(album.cover);
+        await session.commitTransaction();
+        session.endSession();
+        res.status(200).json({ success: true, message: 'Album deleted successfully' });
+    }
+    catch (err) {
+        await session.abortTransaction();
+        session.endSession();
+        console.error('Error deleting album:', err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 }
